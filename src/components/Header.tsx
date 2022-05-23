4ecc5dd06bb1d49280283f7b155e2b0dfad99d71
Alexandr <alexandrgorr@gmail.com>
2022-05-23 16:51:59 +0300

    added block component

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://i.pinimg.com/474x/39/b5/61/39b5613f275faa3b8e2722aa3925f655.jpg"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMusc_3mN25tyiS62YmmJZG9HylhqOFWsKFQ&usqp=CAU"
+          alt="img_photo"
+        />
+      </div>
+      <div>ava + description</div>
+      <div>
+        My posts
+        <div>New post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
