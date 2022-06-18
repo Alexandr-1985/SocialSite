e6c99d795891943d5083ec632cd38764e0280f87
Alexandr <alexandrgorr@gmail.com>
2022-06-18 11:39:26 +0300

    change storeProvider to provider in index, without TS

--- src/StoreContext.tsx
+++ src/StoreContext.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
+const PropsStoreContextProvider{
+  
+}
+
 const StoreConext = React.createContext(null);
+
+export const Provider = (props: PropsStoreContextProvider) => {
+  return (
+    <StoreConext.Provider value={props.store}>
+      {props.children}
+    </StoreConext.Provider>
+  );
+};
 
 export default StoreConext;
--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import store from "./redux/redux-store";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
-import StoreConext from "./StoreContext";
+import StoreConext, { Provider } from "./StoreContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -15,13 +15,13 @@ export let rerenderEntireTree = (state: any) => {
   root.render(
     <React.StrictMode>
       <BrowserRouter>
-        <StoreConext.Provider value={store}>
+        <Provider store={store}>
           <App
           // state={store.getState()}
           // dispatch={store.dispatch.bind(store)}
           // store={store}
           />
-        </StoreConext.Provider>
+        </Provider>
       </BrowserRouter>
     </React.StrictMode>
   );
